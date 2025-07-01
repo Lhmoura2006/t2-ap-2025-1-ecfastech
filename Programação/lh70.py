8b61c81b67e3fe91e723919f03ddf8b410151982
Lhmoura2006 <luizmoura20062006@gmail.com>
2025-07-01 16:58:50 -0700

    Update lh70.py

---
+++ Programação/lh70.py
@@ -0,0 +1,10 @@
+import random
+import string
+def gerar_senha(comprimento, incluir_numeros=True, incluir_simbolos=False):
+    caracteres = string.ascii_letters
+    if incluir_numeros:
+        caracteres += string.digits
+    if incluir_simbolos:
+        caracteres += "!@#$%^&*"
+    senha = ''.join(random.choice(caracteres) for _ in range(comprimento))
+    return senha
