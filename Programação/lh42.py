3860cd611aa7c37f6efee73eb88429505f8ac8d2
Lhmoura2006 <luizmoura20062006@gmail.com>
2025-07-01 16:50:31 -0700

    Update lh42.py

---
+++ Programação/lh42.py
@@ -0,0 +1,17 @@
+contadores = [0,0,0,0]  # 0-25, 26-50, 51-75, 76-100
+while True:
+    num = int(input("Número: "))
+    if num < 0:
+        break
+    if 0 <= num <= 25:
+        contadores[0] += 1
+    elif 26 <= num <= 50:
+        contadores[1] += 1
+    elif 51 <= num <= 75:
+        contadores[2] += 1
+    elif 76 <= num <= 100:
+        contadores[3] += 1
+print(f"[0-25]: {contadores[0]}")
+print(f"[26-50]: {contadores[1]}")
+print(f"[51-75]: {contadores[2]}")
+print(f"[76-100]: {contadores[3]}")
